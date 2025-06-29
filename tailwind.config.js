829abf976902eebbf4409a036f961903e3035c8b
alaaeallam <alaaeallam@gmail.com>
2025-06-29 20:59:01 +0300

    change num font3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/ ** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    'text-4xl',
+    'text-6xl',
   ],
   theme: {
     extend: {},
